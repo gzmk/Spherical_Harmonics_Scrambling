cf686f68fd5db0d29a99d9b8ef789ebbaaef3c89
gzmk <gizemkucukoglu@gmail.com>
2015-03-25 17:49:27 -0400

    forward and inverse transform
    need to be corrected, inv transform not working properly right now

--- forward_sft.m
+++ forward_sft.m
@@ -15,7 +15,7 @@ lf_bw_sm = imresize(lf_bw, [128 256]);
 % latitude -> theta is the angle from the z-axis (0,pi)
 % longitude -> phi is the angle in the xy-plane from the x-axis (0,2*pi)
 latstep = pi/nlat;
-longstep = 2*pi/nlong;
+longstep = 2*pi/nlong*2;
 
 lats = (latstep/2):latstep:(pi-(latstep/2));
 longs = 0:longstep:(2*pi-longstep);
@@ -24,8 +24,9 @@ longs = 0:longstep:(2*pi-longstep);
 % gridpts = [phi(:) theta(:)];
 % wts = getVoronoiWeights(gridpts);
 % Nord = 50;
-% xform = leastSquaresSHT(Nord,lfgsm(:),gridpts,'complex',wts);
+% xform = leastSquaresSHT(Nord,lf_bw_sm(:),gridpts,'complex',wts);
 
+%% Forward transform
 j = 8;
 m = 4;
 fprintf('\n\n\n****************\nTesting with j=%g, m=%g:\n',j,m)
@@ -41,3 +42,11 @@ else
     fprintf('All coefficients are negligible!\n')
 end
 fprintf('----------------------\n');
+
+%% inverse transform
+
+tic,result=InvFST_semi_fly_mex(SphHRep2FSTRep(SphHRep));result_time=toc;
+tic,direct=Ylm(j,m,theta,phi);direct_time=toc;
+
+fprintf(['j=%g, m=%g: error=%g\t\tInverse transform time = %g, Single ' ...
+    'Ylm time = %g\n'],j,m,max(max(abs(result-direct))),result_time,direct_time)
